8a50a0f34127cbc311116d934196c716e100aae6
Anuj Agarwal <a.anuj1405@gmail.com>
2015-12-07 20:41:04 +0530

    dd

---
+++ app/routes.js
@@ -0,0 +1,10 @@
+// app/routes.js
+module.exports = function(app) {
+
+app.get('/aa', function(request, response) {
+  response.send('Hello World!')
+})
+}
+
+// route middleware to make sure
+
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+require('./app/routes.js')(app);
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })

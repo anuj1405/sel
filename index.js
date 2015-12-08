4930a579b1c13e984d78e9d053dc27751bdfde94
Anuj Agarwal <a.anuj1405@gmail.com>
2015-12-08 09:56:31 +0530

    dd

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-app.use('view engine', 'ejs')
+
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
-
 
 
 app.listen(app.get('port'), function() {

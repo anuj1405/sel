c12b7a41d18e90ba665e435ce63ff88f414ac375
Anuj Agarwal <a.anuj1405@gmail.com>
2015-12-10 18:47:43 +0530

    anu1j

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
-
+app.engine('html', require('ejs').renderFile);
 
 require('./app/routes.js')(app);
 

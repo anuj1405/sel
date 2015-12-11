adc7e29d78501c2ff8dcf9f5fcf9b8887df32c93
Anuj Agarwal <a.anuj1405@gmail.com>
2015-12-11 19:11:04 +0530

    op

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-	'url' : 'mongodb://<Anuj>:<legalman*123>@ds027295.mongolab.com:27295/legalman' 
+	'url' : 'mongodb://Anuj:legalman*123@ds027295.mongolab.com:27295/legalman' 
 };
 

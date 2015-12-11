5048d4707c64f1a5f842a340b95db8b27f227ad1
Anuj Agarwal <a.anuj1405@gmail.com>
2015-12-11 19:07:54 +0530

    sio

---
+++ config/database.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+	'url' : 'mongodb://<Anuj>:<legalman*123>@ds027295.mongolab.com:27295/legalman' 
+};
+
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
+var mongoose = require('mongoose')
 var app = express()
+var configDB = require('./config/database.js')
 
+mongoose.connect(configDB.url); // connect to our database
 app.set('port', (process.env.PORT || 5000))
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))

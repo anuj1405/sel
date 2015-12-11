cc77c853ac26921545c1317e62c7f8e379108092
Anuj Agarwal <a.anuj1405@gmail.com>
2015-12-11 13:42:17 +0530

    aa

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,12 @@ module.exports = function(app) {
 app.get('/', function(req, res) {
 	res.render('homepage.html'); 
 });
+app.get('/conact', function(req, res) {
+	res.render('contact.html'); 
+});
+app.get('/partnership', function(req, res) {
+	res.render('partnership.html'); 
+});
 }
 
 // route middleware to make sure
